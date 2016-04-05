4463e2275e722d6db11fca837909c12806353976
stefannikolicsap <stefan.nikolic@sap.com>
2016-04-05 14:12:11 +0200

    Init layout of movies for mobile and init hamburger menu

--- app/genres/controllers/genres-ctrl.js
+++ app/genres/controllers/genres-ctrl.js
@@ -1,4 +1,7 @@
 app.controller('GenresCtrl', ['$scope', 'Restangular', '$location', function($scope, Restangular, $location){
+
+	$scope.hamMenuOpened = false;
+
 	Restangular.setBaseUrl('https://api.themoviedb.org/3/genre/movie');
 	var params = {api_key: "1fd95566952c08b6ecf132ae47c16a33"};
 	var query = Restangular.all('list');
@@ -10,6 +13,10 @@ app.controller('GenresCtrl', ['$scope', 'Restangular', '$location', function($sc
 	$scope.changeGenre = function () {
 		console.log('Genre change');
 		$location.path('/genre/' + $scope.genre);
+	};
+
+	$scope.openHamMenu = function () {
+		$scope.hamMenuOpened = !$scope.hamMenuOpened;
 	};
 
 }])
